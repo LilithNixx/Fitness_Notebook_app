38c4266b501b12c337944ee19ac04272a40b6b23
Daniela Sol <andromeda0209@gmail.com>
2025-08-09 19:16:01 -0300

    cambio de direccion de login

--- tp5-frontend/src/pages/Login.js
+++ tp5-frontend/src/pages/Login.js
@@ -13,7 +13,7 @@ function Login() {
     console.log("🔍 Se presionó el botón y se disparó handleSubmit");
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/login', {
+      const response = await fetch(`${API_BASE}/api/login}}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, contraseña }),
